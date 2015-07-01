9415148f3fc6fc9ab9e016314d554816e808676c
Edoardo Marcora <edoardo.marcora@gmail.com>
2015-07-01 01:08:59 -0700

    edit guid

--- Launchkey49.control.js
+++ Launchkey49.control.js
@@ -1,6 +1,6 @@
 loadAPI(1);
 
-host.defineController("Novation", "Launchkey 49 (Updated)", "1.0", "40635400-6cfb-11e4-9803-0800200c9a66");
+host.defineController("Novation", "Launchkey 49", "1.0", "4e82aa1b-78fd-488b-8a73-8d7ebdf4fc77");
 host.defineMidiPorts(2, 2);
 host.addDeviceNameBasedDiscoveryPair(["Launchkey 49 Launchkey MIDI", "Launchkey 49 Launchkey InControl"], ["Launchkey 49 Launchkey MIDI", "Launchkey 49 Launchkey InControl"]);
 
